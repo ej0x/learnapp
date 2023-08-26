1e96b56a2a1aa1caadc33f8d453098ad068655f8
ej0x <ejas01@outlook.com>
2023-08-26 19:42:30 +0530

    navigator setup

--- lib/pages/description_page.dart
+++ lib/pages/description_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learnapp/class/item_class.dart';
+import 'package:learnapp/core/constants.dart';
 
 class DescriptionPage extends StatelessWidget {
   const DescriptionPage({
@@ -13,6 +14,23 @@ class DescriptionPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(box.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(kDouble10),
+          child: Column(
+            children: [
+              Image.asset(box.imagePath),
+              FittedBox(
+                child: Text(
+                  box.title,
+                  style: const TextStyle(
+                      fontSize: 200, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
