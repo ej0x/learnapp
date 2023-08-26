f8ba03e0367138d7765bd92a2035d2df9bc387ef
ej0x <ejas01@outlook.com>
2023-08-26 15:12:27 +0530

    new widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,8 +9,54 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter App"),
       ),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      Image.asset('images/rocket.png'),
+                      const Text(
+                        "Title",
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                      const Text("Description"),
+                    ],
+                  )),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('images/space.png'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('images/travel.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/yeah.png'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
