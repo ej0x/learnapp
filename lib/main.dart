a43b1a6266f349bce6fc949ca2e24a22f91a7b27
ej0x <ejas01@outlook.com>
2023-08-26 22:27:53 +0530

    widgetTree NavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learnapp/pages/home_page.dart';
+import 'package:learnapp/widget/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const WidgetTree(),
     );
   }
 }
