9c9382ab451ab14f7e28582e809ad2a6151694c9
ej0x <ejas01@outlook.com>
2023-08-26 19:04:26 +0530

    description page setup

--- lib/pages/description_page.dart
+++ lib/pages/description_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:learnapp/class/item_class.dart';
 
 class DescriptionPage extends StatelessWidget {
-  const DescriptionPage({super.key});
+  const DescriptionPage({
+    super.key,
+    required this.box,
+  });
 
+  final ItemClass box;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(box.title),
+      ),
     );
   }
 }
--- lib/widget/card_widget.dart
+++ lib/widget/card_widget.dart
@@ -18,7 +18,9 @@ class CardWidget extends StatelessWidget {
           context,
           MaterialPageRoute(
             builder: (context) {
-              return const DescriptionPage();
+              return DescriptionPage(
+                box: box,
+              );
             },
           ),
         );
