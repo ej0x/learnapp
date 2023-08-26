edc9ea9de658d00088ff788f8d23291eee0a9fa6
ej0x <ejas01@outlook.com>
2023-08-26 22:26:55 +0530

    widgetree NavBar

---
+++ lib/widget/widget_tree.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:learnapp/pages/home_page.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomePage(),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
