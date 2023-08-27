e3bb38d29bf7c6ab3099a9e8bebdfa177ff00623
ej0x <ejas01@outlook.com>
2023-08-27 13:47:04 +0530

    dark mode button added

--- lib/pages/profile_papge.dart
+++ lib/pages/profile_papge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learnapp/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -8,6 +9,33 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
+      ),
+      body: const Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/yeah.png'),
+            radius: 60,
+          ),
+          SizedBox(
+            height: kDouble20,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Flutter App'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('info@E-corp.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text('E-Corp.com'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.dark_mode),
       ),
     );
   }
